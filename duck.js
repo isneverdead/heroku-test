fecf45512a2f759be7bc2eb499f6f1d474db77ce
isneverdead <isneverdead@github.com>
2020-04-01 22:39:32 +0700

    update duck.js

--- duck.js
+++ duck.js
@@ -1,6 +1,6 @@
 const webdriver = require('selenium-webdriver');
 // const chrome = require(selenium-webdriver/chrome);
-
+console.log("masuk ke function")
 (async function getduck() {
   // let driver = await new Builder().forBrowser('chrome').build();
   let chromeOptions = await new webdriver.setChromeOptions()
@@ -8,12 +8,14 @@ const webdriver = require('selenium-webdriver');
     .headless()
     .addArguments('disable-dev-shm-usage')
     .addArguments('no-sandbox');
+    console.log("chrome option done")
   
   let driver = await new webdriver.Builder()
     .forBrowser('chrome')
     .execute('/app/.chromedriver/bin/chromedriver')
     .setChromeOptions(chromeOptions)
     .build();
+    console.log("build chrome done")
   try {
     // Navigate to Url
     await driver.get('https://duckduckgo.com');
